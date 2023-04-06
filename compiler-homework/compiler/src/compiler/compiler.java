b16f4e73a42185676fc3b9ecf5301392fb09b029
Ruben Hernandez <rubenh_2bdrums@yahoo.com>
2023-04-06 12:57:48 -0700

    Add files via upload

---
+++ compiler-homework/compiler/src/compiler/compiler.java
@@ -0,0 +1,12 @@
+package compiler;
+
+
+public class compiler {
+
+    public static void main(String[] args) {
+
+       int[] data = new int[5];
+       data[5] = 9;
+       System.out.println(data[5]);
+    }
+}
